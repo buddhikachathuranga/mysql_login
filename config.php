f43681b6ee73ff4886f3db7001b8c323bcd6e727
Chathu pathirana <buddhika.chathuranga@serverclub.lk>
2023-12-10 02:38:58 +0530

    update

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 define("DB_SERVER", "localhost");
-define("DB_USERNAME", "buddhika_user");
-define("DB_PASSWORD", "a3vpD106^");
-define("DB_NAME", "buddhika_db");
+define("DB_USERNAME", "");
+define("DB_PASSWORD", "");
+define("DB_NAME", "");
 
 # Connection
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
